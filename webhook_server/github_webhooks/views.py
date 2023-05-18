407c377299facd86b1f086440145c6c6f03be308
hens2013 <hens2013@gmail.com>
2023-05-18 14:07:36 +0300

    adding requirements.txt file

--- webhook_server/github_webhooks/views.py
+++ webhook_server/github_webhooks/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def webhook_handler(payload):
-    print(payload)
+    print(type(payload))
     # print('-' * 100)
     # print(json.loads(request.body))
     # print('-' * 100)
