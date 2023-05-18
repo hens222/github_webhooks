faec047aeaa5886916d2f0b2d5b2227582f6f32b
hens2013 <hens2013@gmail.com>
2023-05-18 13:41:43 +0300

    adding requirements.txt file

--- webhook_server/github_webhooks/urls.py
+++ webhook_server/github_webhooks/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import webhook_handler, index
 
 urlpatterns = [
-    path('webhook/', webhook_handler, name='webhook_handler'),
+    #path('webhook/', webhook_handler, name='webhook_handler'),
+    path(r'^api/hello/$', webhook_handler, name='webhook_handler'),
+
     path('', index, name='index'),
 ]
--- webhook_server/github_webhooks/views.py
+++ webhook_server/github_webhooks/views.py
@@ -6,51 +6,52 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
-def webhook_handler(request, event):
-    print(event)
+def webhook_handler(request):
+    # print(event)
+    # # if request.method == 'POST':
+    # #     print('-' * 100)
+    # #     print(request)
+    # #     print('-' * 100)
+    # #     payload = json.loads(request.body)
+    # #     # Handle the webhook payload here
+    # #     # Example: process the payload and perform necessary actions
+    # #     event_type = request.headers.get('X-GitHub-Event')
+    # #     print(f'Received {event_type} event: {payload}')
+    # #     # Return a response
+    # #     return HttpResponse(status=200)
+    # # else:
+    # #     return HttpResponse(status=405)
+    # print('-' * 100)
+    # print(json.loads(request.body))
+    # print('-' * 100)
     # if request.method == 'POST':
-    #     print('-' * 100)
-    #     print(request)
-    #     print('-' * 100)
-    #     payload = json.loads(request.body)
-    #     # Handle the webhook payload here
-    #     # Example: process the payload and perform necessary actions
-    #     event_type = request.headers.get('X-GitHub-Event')
-    #     print(f'Received {event_type} event: {payload}')
-    #     # Return a response
-    #     return HttpResponse(status=200)
+    #
+    #     csrf_token = request.GET.get('csrfmiddlewaretoken', '')
+    #     if csrf_token == get_token(request):
+    #         payload = json.loads(request.body)
+    #         # Handle the webhook payload here
+    #         # Example: process the payload and perform necessary actions
+    #         event_type = request.headers.get('X-GitHub-Event')
+    #         print(f'Received {event_type} event: {payload}')
+    #         # Return a response as JSON
+    #         response_data = {
+    #             'status': 'success',
+    #             'message': 'Webhook received and processed successfully.'
+    #         }
+    #         return JsonResponse(response_data, status=200)
+    #     else:
+    #         response_data = {
+    #             'status': 'error',
+    #             'message': 'CSRF verification failed.'
+    #         }
+    #         return JsonResponse(response_data, status=403)
     # else:
-    #     return HttpResponse(status=405)
-    print('-' * 100)
-    print(json.loads(request.body))
-    print('-' * 100)
-    if request.method == 'POST':
-
-        csrf_token = request.GET.get('csrfmiddlewaretoken', '')
-        if csrf_token == get_token(request):
-            payload = json.loads(request.body)
-            # Handle the webhook payload here
-            # Example: process the payload and perform necessary actions
-            event_type = request.headers.get('X-GitHub-Event')
-            print(f'Received {event_type} event: {payload}')
-            # Return a response as JSON
-            response_data = {
-                'status': 'success',
-                'message': 'Webhook received and processed successfully.'
-            }
-            return JsonResponse(response_data, status=200)
-        else:
-            response_data = {
-                'status': 'error',
-                'message': 'CSRF verification failed.'
-            }
-            return JsonResponse(response_data, status=403)
-    else:
-        response_data = {
-            'status': 'error',
-            'message': 'Invalid request method.'
-        }
-        return JsonResponse(response_data, status=405)
+    #     response_data = {
+    #         'status': 'error',
+    #         'message': 'Invalid request method.'
+    #     }
+    #     return JsonResponse(response_data, status=405)
+    return HttpResponse('pong')
 
 
 def index(request):
