668f5a91265c627189473b4c35ff0339bdcad46d
hens2013 <hens2013@gmail.com>
2023-05-18 13:05:15 +0300

    adding requirements.txt file

--- webhook_server/github_webhooks/views.py
+++ webhook_server/github_webhooks/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def webhook_handler(request):
     if request.method == 'POST':
         payload = json.loads(request.body)
