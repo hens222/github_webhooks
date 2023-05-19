7890e0a3e1844c71a4f97221b42461135aee4ed7
hens2013 <hens2013@gmail.com>
2023-05-19 14:30:56 +0300

    adding pull request model

--- webhook_server/github_webhooks/admin.py
+++ webhook_server/github_webhooks/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import PullRequest
 
+
 @admin.register(PullRequest)
 class PullRequestAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'state', 'created_at', 'updated_at']
