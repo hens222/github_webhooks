da2b7c1995d1b4962021f24cd7fef46dc585d289
hens2013 <hens2013@gmail.com>
2023-05-19 15:09:49 +0300

    adding pull request model

--- webhook_server/github_webhooks/views.py
+++ webhook_server/github_webhooks/views.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
-from django.http import HttpResponse, JsonResponse
-import json
-
-from django.middleware.csrf import get_token
+from django.http import HttpResponse
+import logging
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import PullRequest
@@ -16,36 +14,43 @@ def check_pull_request_exists(pk):
 @csrf_exempt
 def webhook_handler(request):
     if request.method == 'POST':
-        payload = json.loads(request.body)
-        # Handle the webhook payload here
-        # Example: process the payload and perform necessary actions
+        payload = request.json()
+
         event_type = request.headers.get('X-GitHub-Event')
         if event_type == 'pull_request':
             pull_request = payload['pull_request']
             id = pull_request['id']
             action = payload['action']
             state = pull_request['state']
+            updated_at = None
+
             if pull_request['updated_at']:
                 updated_at = datetime.strptime(pull_request['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
-            else:
-                updated_at = None
-            if not check_pull_request_exists(id):
-                PullRequest.objects.create(
-                    action=action,
+
+            try:
+                obj, created = PullRequest.objects.get_or_create(
                     id=id,
-                    url=pull_request['html_url'],
-                    state=state,
-                    title=pull_request['title'],
-                    body=pull_request['body'],
-                    created_at=datetime.strptime(pull_request['created_at'], "%Y-%m-%dT%H:%M:%SZ"),
-                    updated_at=updated_at,
-                    merge_commit_sha=pull_request['merge_commit_sha'],
-                    user=pull_request['user']['login'],
+                    defaults={
+                        'action': action,
+                        'url': pull_request['html_url'],
+                        'state': state,
+                        'title': pull_request['title'],
+                        'body': pull_request['body'],
+                        'created_at': datetime.strptime(pull_request['created_at'], "%Y-%m-%dT%H:%M:%SZ"),
+                        'updated_at': updated_at,
+                        'merge_commit_sha': pull_request['merge_commit_sha'],
+                        'user': pull_request['user']['login'],
+                    }
                 )
-            else:
-                PullRequest.objects.filter(id=id).update(action=action, state=state,
-                                                         updated_at=updated_at)
-        # Return a response
+
+                if not created:
+                    obj.action = action
+                    obj.state = state
+                    obj.updated_at = updated_at
+                    obj.save()
+
+            except PullRequest.DoesNotExist:
+                logging.error(f"PullRequest with id={id} does not exist: {str(e)}")
+
         return HttpResponse(status=200)
-    else:
-        return HttpResponse(status=405)
+    return HttpResponse(status=405)
