4f214d663953567858d9b969d41bffe9d4fe42ca
hens2013 <hens2013@gmail.com>
2023-05-19 13:56:17 +0300

    adding pull request model

--- webhook_server/github_webhooks/views.py
+++ webhook_server/github_webhooks/views.py
@@ -20,6 +20,8 @@ def webhook_handler(request):
         # Handle the webhook payload here
         # Example: process the payload and perform necessary actions
         event_type = request.headers.get('X-GitHub-Event')
+        print(ACTION_CHOICES)
+        print(type(ACTION_CHOICES))
         if event_type == 'pull_request':
             pu_number = payload['number']
             action = ACTION_CHOICES[[payload['action']]]
