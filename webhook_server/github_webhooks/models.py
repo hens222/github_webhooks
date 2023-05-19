4c70384b5f94e3d24735218a3bd76b1c8dccb383
hens2013 <hens2013@gmail.com>
2023-05-19 14:39:11 +0300

    adding pull request model

--- webhook_server/github_webhooks/models.py
+++ webhook_server/github_webhooks/models.py
@@ -14,7 +14,6 @@ class PullRequest(models.Model):
     ]
     action = models.CharField(max_length=10, choices=ACTION_CHOICES)
     id = models.PositiveIntegerField(primary_key=True)
-    number = models.PositiveIntegerField()
     url = models.URLField()
     state = models.CharField(max_length=10, choices=STATE_CHOICES)
     title = models.CharField(max_length=255)
